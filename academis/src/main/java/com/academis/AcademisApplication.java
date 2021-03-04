702983c5f447546b70374729f70edab99a951480
clayton7000 <80071108+clayton7000@users.noreply.github.com>
2021-03-04 18:22:28 -0300

    1 upload

---
+++ academis/src/main/java/com/academis/AcademisApplication.java
@@ -0,0 +1,13 @@
+package com.academis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademisApplication.class, args);
+	}
+
+}
---
+++ academis/src/test/java/com/academis/AcademisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
