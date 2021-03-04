6b74d71c17bfe945ef428550175895816e853971
clayton7000 <clayton_700_700@hotmail.com>
2021-03-04 20:17:29 -0300

    1 commit
    teste

---
+++ academis/src/main/java/com/academis/model/Usuario.java
@@ -0,0 +1,44 @@
+package com.academis.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+	@Id
+	private String matricula;
+	private String nome;
+	private String login;
+	private String senha;
+	private int tipo;
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public int getTipo() {
+		return tipo;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+}
